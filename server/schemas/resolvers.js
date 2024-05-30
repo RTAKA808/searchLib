c3c79f8e6b81740378c943c0b1b2e7bbdbcdc14c
RTAKA808 <106231361+RTAKA808@users.noreply.github.com>
2024-05-30 00:31:59 -0700

    backend pretty much done

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,23 +5,57 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers={
     Query:{
-        user: async (parent, { username, email }) => {
-            return User.findOne({ username, email });
-        },
+        user: async (parent, { id, username  }) => {
+            return User.findOne({ $or: [{_id: id}, {username: username}]  });
+        }
+    },
 
+    Mutation:{
+        createUser: async (parent,{username,email,password})=>{
+            const user=awaitUser.create({username,email,password});
+            const token=signToken(user);
+            return{ token,user};
+        },
+        login: async (parent, {username, email, password }) => {
+            const user = await User.findOne({ $or: [{username},{email}] });
+    
+            if (!user) {
+            throw AuthenticationError;
+            }
+    
+            const correctPw = await user.isCorrectPassword(password);
+    
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+    
+            const token = signToken(user);
+    
+            return { token, user };
+        },
+        saveBook: async (parent, { bookId, authors, description, title, image, link }, context)=>{
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: user._id },
+                    { $addToSet: { savedBooks: { bookId, authors, description, title, image, link } } },
+                    { new: true, runValidators: true }
+                  );
+                  return updatedUser;
+            }
+            throw AuthenticationError;
+        },
+        deleteBook: async (parent, { bookId }, context)=>{
+          if (context.user){
+            const updatedUser=await User.findOneAndUpdate(
+              { _id: user._id },
+              { $pull: { savedBooks: { bookId } } },
+              { new: true }
+            );
+            return updatedUser;
+          }
+        },
+      }
     }
 
+module.exports=resolvers;
 
-
-
-
-
-
-
-
-
-
-
-
-
-}
--- server/schemas/typeDef.js
+++ server/schemas/typeDef.js
@@ -1,4 +1,36 @@
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    savedBooks: [Book]
+  }
 
-module.exports = { typeDefs, resolvers };
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: String
+    user: User
+  }
+
+  type Query {
+    getSingleUser(id: ID, username: String): User
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(username: String, email: String, password: String!): Auth
+    saveBook(bookId: String!, authors: [String], description: String, title: String, image: String, link: String): User
+    deleteBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
+
